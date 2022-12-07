85bc43fb5c92546fc9da44481881ece08f6a0e60
rafarosen <109546448+rafarosen@users.noreply.github.com>
2022-12-06 21:46:30 -0600

    Add files via upload

---
+++ Entrega_Final/js/products.js
@@ -0,0 +1,35 @@
+const productos = [
+    { 
+        id: 1,
+        nombre: "Pelotas",
+        precio: 7,
+        img: 
+            "https://cdn.shopify.com/s/files/1/1218/5228/products/WilsonJumbo9InflatableTennisBall-Yellow_1000x.jpg?v=1635518254",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Raqueta",
+        precio: 250,
+        img: 
+            "https://cdn.shopify.com/s/files/1/1218/5228/products/WilsonProStaffRF97V13_1000x.png?v=1600645837",
+        cantidad: 1,
+    },
+    { 
+        id: 3,
+        nombre: "Raquetero",
+        precio: 120,
+        img: 
+            "https://cdn.shopify.com/s/files/1/1218/5228/products/Wilson-Tour-12R-WR8011201001_1000x.jpg?v=1628282125",
+        cantidad: 1,
+    },
+    { 
+        id: 4,
+        nombre: "Zapatillas",
+        precio: 120,
+        img: 
+            "https://cdn.shopify.com/s/files/1/1218/5228/products/M-CZ0220-009-0_1000x.jpg?v=1659364944",
+        cantidad: 1,
+    },
+    
+];
